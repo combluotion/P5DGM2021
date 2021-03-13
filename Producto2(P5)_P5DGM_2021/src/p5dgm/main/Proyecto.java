15743328d16fb84a6c4d5f1edc8a8c682899c357
Diego Romero <Diego Romero@Dieguito>
2021-03-13 15:22:43 +0100

    New class ProyectoDAO e informada clase estática Proyecto.
    Con atributos y funciones principales de creación y obtención de datos.

--- Producto2(P5)_P5DGM_2021/src/p5dgm/main/Proyecto.java
+++ Producto2(P5)_P5DGM_2021/src/p5dgm/main/Proyecto.java
@@ -1,9 +1,66 @@
 package p5dgm.main;
 
+import java.util.Date;
+
 public class Proyecto {
 
-	public Proyecto() {
-		// TODO Auto-generated constructor stub
+	public int idProyecto;
+	public String nombreProyecto;
+	public String tipoProyecto;
+	public String pais;
+	public Date fechaInicio;
+	public Date fechaFin;
+	public float financiacionAportada;
+	
+	
+	public int getIdProyecto() {
+		return idProyecto;
+	}
+	public void setIdProyecto(int idProyecto) {
+		this.idProyecto = idProyecto;
+	}
+	public String getNombreProyecto() {
+		return nombreProyecto;
+	}
+	public void setNombreProyecto(String nombreProyecto) {
+		this.nombreProyecto = nombreProyecto;
+	}
+	public String getTipoProyecto() {
+		return tipoProyecto;
+	}
+	public void setTipoProyecto(String tipoProyecto) {
+		this.tipoProyecto = tipoProyecto;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public float getFinanciacionAportada() {
+		return financiacionAportada;
+	}
+	public void setFinanciacionAportada(float financiacionAportada) {
+		this.financiacionAportada = financiacionAportada;
 	}
 
+	public Proyecto GuardarProyecto() {
+		return this;
+	}
+
+	public Proyecto ObtenerProyecto() {
+		return this;
+	}
 }
