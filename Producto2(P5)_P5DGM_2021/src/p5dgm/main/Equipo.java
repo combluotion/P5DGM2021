f772af78cf4a835b71b13946b2db74bc5f97dd80
gerard <gerard@gerard-EZbook>
2021-03-13 21:04:04 +0100

    class equipo update

--- Producto2(P5)_P5DGM_2021/src/p5dgm/main/Equipo.java
+++ Producto2(P5)_P5DGM_2021/src/p5dgm/main/Equipo.java
@@ -1,9 +1,61 @@
 package p5dgm.main;
+import java.util.Date;
 
 public class Equipo {
 
-	public Equipo() {
-		// TODO Auto-generated constructor stub
+	public int id;
+	public String nombre;
+	public Date fechaNacimiento;
+	public String direccion;
+	public String telefono;
+	public String delegacion;
+	
+	public Equipo (int id, String nombre, Date fechaNacimiento, String direccion,
+			String telefono, String delegacion){
+		this.id = id;
+		this.nombre = nombre;
+		this.fechaNacimiento = fechaNacimiento;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.delegacion = delegacion;
 	}
-
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDelegacion() {
+		return delegacion;
+	}
+	public void setDelegacion(String delegacion) {
+		this.delegacion = delegacion;
+	}
+	
+	
 }
