19af92be1808be460d6f2ef7973cd88bba61ddf7
MCarmen72 <mreinah@uoc.edu>
2021-03-30 12:39:58 +0200

    Clase socio actualizada con Factory y JUnit

---
+++ Producto2(P5)_P5DGM_2021/src/p5dgm/dao/xml/XMLSocioDAOTest.java
@@ -0,0 +1,60 @@
+package p5dgm.dao.xml;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import p5dgm.dao.ProyectoDAO;
+import p5dgm.dao.SocioDAO;
+import p5dgm.main.Proyecto;
+import p5dgm.main.Socio;
+
+
+
+
+public class XMLSocioDAOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testInsertar() throws ParseException {
+		Socio socio = new Socio(1,"RAmon Gracia Serra","Calle Mayor 50","658965478","Madrid","menual",150);
+		SocioDAO socioDAO = new XMLSocioDAO();
+		try {
+			socioDAO.insertar(socio);
+			assertTrue(true);
+		} catch (IOException e){
+			e.printStackTrace();			
+			fail(e.getMessage());
+		}
+		
+	}
+
+	@Test
+	public void testModificar() throws ParseException {
+		SocioDAO DAOmodificar = new XMLSocioDAO();
+        Socio modificar  = new Socio(1,"RAmon Gracia Serra","Calle Mayor 50","658965478","Madrid","trimestral",550);
+        try {
+			DAOmodificar.modificar(modificar);
+			assertTrue(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+}
+
+	
--- Producto2(P5)_P5DGM_2021/src/p5dgm/main/Socio.java
+++ Producto2(P5)_P5DGM_2021/src/p5dgm/main/Socio.java
@@ -1,9 +1,93 @@
 package p5dgm.main;
 
+import java.util.Date;
+import java.util.List;
+
 public class Socio {
 
-	public Socio() {
-		// TODO Auto-generated constructor stub
+	//Atributos
+	
+			public int idSocio;
+			public String nombreSocio;
+			public String direccion;
+			public String telefono;
+			public String delegacion;
+			public String tipoCuota;
+			public float ImporteCuota;
+			
+	//Metodo constructor
+			
+			public Socio (int idSocio, String nombreSocio, String direccion, String telefono, String delegacion,
+				String tipoCuota, float ImporteCuota) {
+				
+				this.idSocio = idSocio;
+				this.nombreSocio = nombreSocio;
+				this.telefono = telefono;
+				this.delegacion = delegacion;
+				this.tipoCuota = tipoCuota;
+				this.ImporteCuota = ImporteCuota;
+			}
+			
+	//Metodos getters and setters
+			
+
+
+			public int getIdSocio() {
+				return idSocio;
+			}
+
+			public void setIdSocio(int idSocio) {
+				this.idSocio = idSocio;
+			}
+
+			public String getNombreSocio() {
+				return nombreSocio;
+			}
+
+			public void setNombreSocio(String nombreSocio) {
+				this.nombreSocio = nombreSocio;
+			}
+
+			public String getDireccion() {
+				return direccion;
+			}
+
+			public void setDireccion(String direccion) {
+				this.direccion = direccion;
+			}
+
+			public String getTelefono() {
+				return telefono;
+			}
+
+			public void setTelefono(String telefono) {
+				this.telefono = telefono;
+			}
+
+			public String getDelegacion() {
+				return delegacion;
+			}
+
+			public void setDelegacion(String delegacion) {
+				this.delegacion = delegacion;
+			}
+
+			public String getTipoCuota() {
+				return tipoCuota;
+			}
+
+			public void setTipoCuota(String tipoCuota) {
+				this.tipoCuota = tipoCuota;
+			}
+
+			public float getImporteCuota() {
+				return ImporteCuota;
+			}
+
+			public void setImporteCuota(float importeCuota) {
+				ImporteCuota = importeCuota;
+			}
+			
 	}
 
-}
+
